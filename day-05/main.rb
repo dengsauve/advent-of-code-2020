6c857d4d78e12af3a102360a6c22ca29f738d9ed
Dennis Sauve <dengsauve@yahoo.com>
2020-12-04 21:28:55 -0800

    Day-05 solved

---
+++ day-05/main.rb
@@ -0,0 +1,51 @@
+input = open('./input.txt').readlines
+
+ids = []
+
+input.each do |pass|
+  rows = (0..127)
+  cols = (0..7)
+
+  (0..6).each do |index|
+    operator = pass[index]
+
+    if operator == 'F'
+      rows = rows.each_slice( (rows.size/2.0).round ).to_a[0]
+    elsif operator == 'B'
+      rows = rows.each_slice( (rows.size/2.0).round ).to_a[1]
+    else
+      raise "Invalid Row Operator"
+    end
+  end
+  raise "BS, there's more than one row left" if rows.size > 1
+
+  row = rows[0]
+
+  (7..9).each do |index|
+    operator = pass[index]
+
+    if operator == 'L'
+      cols = cols.each_slice( (cols.size/2.0).round ).to_a[0]
+    elsif operator == 'R'
+      cols = cols.each_slice( (cols.size/2.0).round ).to_a[1]
+    else
+      raise "Invalid Col Operator"
+    end
+  end
+  raise "BS, there's more than one column left" if rows.size > 1
+
+  col = cols[0]
+
+  ids << (row * 8 + col)
+end
+
+puts ids.max
+
+ids.sort!
+
+(0..(ids.size - 2)).each do |index|
+  if ids[index] == ids[index + 1] - 2
+    puts ids[index] + 1, "Should be between #{ids[index]} and #{ids[index + 1]}"
+    exit
+  end
+end
