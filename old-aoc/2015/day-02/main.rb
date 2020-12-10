18b15ee4064a152c92eae381b68859c3c7af6bbf
Dennis Sauve <dengsauve@yahoo.com>
2020-12-09 19:53:20 -0800

    2015 Day-02.2

--- old-aoc/2015/day-02/main.rb
+++ old-aoc/2015/day-02/main.rb
@@ -4,8 +4,18 @@ def get_box_area(l, w, h)
   (2 * l * w) + (2 * w * h) + (2 * h * l)
 end
 
+def get_box_volume(l, w, h)
+  l * w * h
+end
+
 def get_slack(dims)
+  dims.sort!
   dims[0] * dims[1]
+end
+
+def get_box_short_perimeter(dims)
+  dims.sort!
+  (2 * dims[0]) + (2 * dims[1])
 end
 
 total_square_feet = 0
@@ -13,7 +23,17 @@ total_square_feet = 0
 input.each do |dims|
   dims = dims.split('x').map { |e| e.to_i }
   total_square_feet += get_box_area(*dims)
-  total_square_feet += get_slack(dims.sort)
+  total_square_feet += get_slack(dims)
 end
 
 puts total_square_feet # 1440710 too low
+
+ribbon_needed = 0
+
+input.each do |dims|
+  dims = dims.split('x').map { |e| e.to_i }
+  ribbon_needed += get_box_short_perimeter(dims)
+  ribbon_needed += get_box_volume(*dims)
+end
+
+puts ribbon_needed
