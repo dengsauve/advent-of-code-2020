2433c921f29c394ac9be941e6ab003c95c464b34
Dennis Sauve <dengsauve@yahoo.com>
2021-10-09 11:52:27 -0700

    Leaving in the brute method, I learned a lot about using hashes (dictionaries) for this type of problem

--- day-15/main.rb
+++ day-15/main.rb
@@ -1,1 +1,35 @@
-input = open('input.txt').readlines
+input = [5,2,8,16,18,0,1]
+indexes = {}
+target = 2020
+
+input[0...-1].each_with_index do |t, i|
+	indexes[t] = i
+end
+
+=begin
+# Find the most recent (starting from end) occurrence of target in array
+# Return 0 if none
+def find_last_instance(target, arr)
+	last_occurence_ix_arr = arr.each_index.select{|i| arr[i] == target}
+	
+	return 0 if last_occurence_ix_arr.count == 0
+	
+	return arr.count - last_occurence_ix_arr.max
+end
+=end
+
+until input.length == target do
+	# puts "input", input.inspect, "indexes", indexes.inspect
+	# gets
+	
+	last_num = input[-1]
+	next_num = 0
+	if indexes.has_key?(last_num)
+		# puts "found key"
+		next_num = input.length - (indexes[last_num] + 1)
+	end
+	indexes[last_num] = input.length - 1
+	input << next_num
+end
+
+puts "The #{target}th number is: #{input[-1]}"
