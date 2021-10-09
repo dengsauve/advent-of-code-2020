9cb2bd61c6d613b55a8766fae54d025c9b3193d4
Dennis Sauve <dengsauve@yahoo.com>
2021-10-08 18:31:42 -0700

    Day 14 completed

--- day-14/main.rb
+++ day-14/main.rb
@@ -69,4 +69,83 @@ values.each do |value|
 end
 puts total
 
-# Part 1 answer: 10035335144067
+## Part 1 answer: 10035335144067
+
+# PART 2
+
+bit_mask = ''
+memory = {}
+
+## Apply bit_mask to 36 bit integer
+def float_mask_bit_string(bit_string, mask)
+	# Get the number of combinations
+	number_of_floats = mask.count('X')
+	
+	puts "Parsing floats, found #{number_of_floats} floats to compute\n\
+Expect #{2 ** number_of_floats} combinations to try"
+	
+	ingredients = [0,1] * number_of_floats
+	combinations = ingredients.combination(number_of_floats).to_a.uniq
+	values = []
+	
+	if combinations.count != 2 ** number_of_floats
+		raise "Error: #{combinations.count} != #{2 ** number_of_floats}"
+	end
+	
+	combinations.each do |f| # f is for float ;) array
+		bit_string_copy = "#{bit_string}"
+		(0..35).each do |index|
+			if mask[index] == '0'
+				# No Change 
+			elsif mask[index] == '1'
+				bit_string_copy[index] = '1'
+			elsif mask[index] = 'X'
+				bit_string_copy[index] = "#{f.pop}"
+			end
+		end
+		values << bit_string_copy
+	end
+	
+	raise("duplicates found") if values.uniq.count != combinations.count
+	
+	return values
+end
+
+# actions
+input.each_with_index do |command, index|
+	puts "\nexecuting command #{index}: #{command}"
+	case command[0..2]
+	when 'mas'
+		bit_mask = command.split()[2]
+	when 'mem'
+		# mem[43805] = 6934
+		# grab the integer for memory location
+		i = command[/\[.*?\]/][1..-2].to_i
+		# write that as a bit_string
+		i_bit_string = decimal_to_36_bit_string(i)
+		# grab the integer for memory insertion
+		value = command.split()[2].to_i
+		# Apply the bit mask (returns array)
+		masked_addresses = float_mask_bit_string(i_bit_string, bit_mask)
+		# Write bit string to memory
+		masked_addresses.each do |address|
+			i = bit_string_to_decimal(address)
+			memory[i] = value
+		end
+	end
+end
+
+puts "\ncalculating grand total"
+
+values = memory.values.flatten
+total = 0
+values.each do |value|
+	total += value
+end
+
+puts total, total == 3817372618036
+
+# 3817372618036 is correct :D
+# 2067229924246400 is too high :/
+# 2577478930633984 is too high :/
+# 2578157945489184
